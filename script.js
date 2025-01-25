53c4522a04051b4cf5968ccd5d3b557fa7c73131
Smitayadav01 <960smitayadav@gmail.com>
2025-01-26 00:18:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// smooth scrolling 
-// - attach loco scroll css
-// -attach locomotive scroll min js 
-// - some code from loco github for js 
-
-// gsap
-//-attach gsap 
-
-
-// scrolltrigger
-
-
-
-
-
-
-
-
-
 const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),   // el means a top level element in html 
     smooth: true
@@ -45,11 +26,7 @@ function firstPageAnim(){
         delay:-1,
         ease: Expo.easeInOut
     })
-}
-
-// jab mouse move ho toh hum log skew kar paaye aur maximum skew and minimum skew define kar paaye 
-// skew matlab ki chapta 
-// jab mouse move ho toh chapta ki value badhe, aur jab mouse chalna band ho jaye to chapta hata lo 
+} 
 
 var timeout;
 
@@ -88,12 +65,6 @@ function  circleMouseFollower(xscale,yscale){
 circleMouseFollower();
 firstPageAnim();
 
-// teeno elem ko select karo uske baad teeno par ek mousemove lagao,
-// jab mousemove ho toh ye pata karo ki mouse kaha par hai,
-// jiska matlab hai mouse ki x and y position pata karo 
-// ab mouse ki x y position ki badle us image ko show karo and us image ko move karo,
-// move karte waqt rotate karo, and jaise jaise mouse tez chale 
-// waise waise rotation bhi tez ho jaye 
 
 document.querySelectorAll(".elem").forEach(function(elem){
     elem.addEventListener("mousemove",function(dets){
